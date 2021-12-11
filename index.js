caaf7b801c7cfca944d745d096c0d5fcbad1993e
LinTuc <linda.olofsson.syned21jon@tucsweden.se>
2021-12-11 14:32:16 +0100

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express= require("express");
 const app = express();
-app.get("/index", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 app.listen(3000);
